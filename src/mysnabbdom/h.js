49d321251729489ed1c557d3fcf2c8a288b63c87
Vsa1968911450 <1968911450@qq.com>
2022-02-18 17:39:08 +0800

    判断新老节点是否为text

--- src/mysnabbdom/h.js
+++ src/mysnabbdom/h.js
@@ -1,25 +1,25 @@
 import vnode from './vnode.js';
-// 调用到时候是下面三种
+// 简单试着写写 只限于调用到时候是下面三种
 // h('div',{},text)
 //h('div',{},[])
 // h('div',{},h())
-export default function(sel,data,c){
+export default function h(sel,data,c){
   // 检查参数个数
-  console.log(arguments)
   if(arguments.length !=3) {
     throw Error('三个参数')
   }
   if(typeof c== 'string' || typeof c == 'number'){
     return vnode(sel,data,undefined,c,undefined)
   } else if(Array.isArray(c)){
+    console.log('1212我来了')
     let children = []
     c.forEach(item=>{
       if(!(typeof item == 'object' && item.hasOwnProperty('sel'))){
         throw Error('传入数组有项不是h函数')
       }
       children.push(item)
-      return vnode(sel,data,children,c,undefined)
     })
+    return vnode(sel,data,children,c,undefined)
   } else if(typeof c == 'object' && c.hasOwnProperty('sel')){
     let children = [c]
     return vnode(sel,data,children,c,undefined)
--- src/mysnabbdom/patch.js
+++ src/mysnabbdom/patch.js
@@ -1,17 +1,47 @@
 import vnode from "./vnode";
 import creatElement from "./creatElement"
-export default function(oldNode,newNode){
+export default function patch(oldNode,newNode){
   // 判断老节点是不是虚拟节点
   console.log(oldNode)
+  console.log(newNode)
   if(oldNode.sel == '' || oldNode.sel === undefined){
     oldNode = vnode(oldNode.tagName.toLowerCase(),{},[],undefined,oldNode)
   }
   // 判断是不是同一个节点
   if(oldNode.key == newNode.key && oldNode.sel == newNode.sel) {
     console.log('同一节点')
+    if(oldNode == newNode) { //内存中相等 
+      return
+    }
+    if(newNode.text != undefined &&(newNode.children == undefined || newNode.children.length == 0)){  //没有子节点且不为空说明有text属性
+      console.log('有text属性') 
+      // 两个text不同
+      if(newNode.text != oldNode.text){
+        oldNode.elm.innerText = newNode.text
+      } else {
+        // 判断有没有children
+        if(oldNode.children != undefined && oldNode.children > 0){
+          // diff
+        } else {
+          // 老 没 新 有
+          // 清空老的节点
+          oldNode.elm.innerHTML = ''
+          let d = newNode.children
+          d.forEach(item=>{
+            let dom = creatElement(item)
+            oldNode.elm.appendChild(dom)
+          })
+          
+          
+        }
+      }
+    }
   } else {
     console.log('butong')
     let newElmemnt =  creatElement(newNode,oldNode.elm)
-    oldNode.elm.parentNode.insertBefore(newElmemnt,oldNode.elm)
+    if(oldNode.elm.parentNode && newElmemnt){
+       oldNode.elm.parentNode.insertBefore(newElmemnt,oldNode.elm)
+    } 
+    oldNode.elm.parentNode.removeChild(oldNode.elm)
   }
 }
